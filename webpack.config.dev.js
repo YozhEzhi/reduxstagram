b39eff85aa39bbfc04cab51ee7f899ce64943197
YozhEzhi <yozhezhi@gmail.com>
2017-04-23 20:16:18 +0300

    Change dist dir

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,7 +8,7 @@ module.exports = {
     './client/reduxstagram'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/build/'
   },
